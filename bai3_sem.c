144ba958b834bf9c222ab6ccd415736be6e2c839
KOS <letrung02082000@gmail.com>
2020-12-20 10:49:55 +0700

    bai 3

--- bai3_sem.c
+++ bai3_sem.c
@@ -1,21 +1,26 @@
 #include<pthread.h>
 #include<stdio.h>
 #include<unistd.h>
+#include<semaphore.h>
+
+sem_t mutexH, mutexO;
 
 void* MakeH(void* arg){
     while (1)
     {
-        sleep(2);
+        sleep(1);
         printf("H\n");
+        sem_post(&mutexH);
     }
     
 }
 
 void* MakeO(void* arg){
-    while (2)
+    while (1)
     {
-        sleep(1);
+        sleep(2);
         printf("O\n");
+        sem_post(&mutexO);
     }
     
 }
@@ -23,7 +28,9 @@ void* MakeO(void* arg){
 void* MakeH2O(void* arg){
     while (1)
     {
-        sleep(1);
+        sem_wait(&mutexH);
+        sem_wait(&mutexO);
+        sem_wait(&mutexH);
         printf("->H2O\n");
     }
     
@@ -31,6 +38,8 @@ void* MakeH2O(void* arg){
 
 int main(void){
     pthread_t p1, p2, p3, p4;
+    sem_init(&mutexH, 1, 0);
+    sem_init(&mutexO, 1, 0);
 
     pthread_create(&p1, NULL, MakeH, NULL);
     pthread_create(&p2, NULL, MakeH, NULL);
@@ -41,5 +50,8 @@ int main(void){
     pthread_join(p2, NULL);
     pthread_join(p3, NULL);
     pthread_join(p4, NULL);
+
+    sem_destroy(&mutexH);
+    sem_destroy(&mutexO);
     return 0;
 }
