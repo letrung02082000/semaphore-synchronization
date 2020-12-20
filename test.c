2ab299441598f63422c4b54232707bc2003f1a17
KOS <letrung02082000@gmail.com>
2020-12-20 19:19:01 +0700

    bai 3

--- test.c
+++ test.c
@@ -1,31 +1,93 @@
-#include<stdio.h>
-#include<pthread.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <semaphore.h>
 #include <unistd.h>
 
-int x = 0;
+sem_t mutex, mutexO, mutexH;
 
-void *threadFn(void* arg){
-    do {
-        x = x + 1;
-        if(x == 20){
-            //sleep(1);
-            x = 0;
-        } else if(x > 20){
-            printf("%d\n", x);
-            break;
-        }
-    } while(1);
+int oxy=0, hidro=0;
+
+pthread_t oxyThread,hidroThread1,hidroThread2;
+
+int MakeH2O(void){
+    static int i=0;
+    i++;
+    if((i%3)==0)
+        printf("->H2O\n");
+    sleep(2);
+    return 0;
 }
 
-int main(void){
-    pthread_t p1, p2;
-    //int x = 0;
+void* oxyFn(void* arg){
 
-    pthread_create(&p2, NULL, threadFn, NULL);
-    pthread_create(&p1, NULL, threadFn, NULL);
+    while(1){
+        sem_wait(&mutex);
 
-    pthread_join(p1, NULL);
-    pthread_join(p2, NULL);
+        oxy+=1;
+
+        if(hidro>=2){
+            sem_post(&mutexH);
+            sem_post(&mutexH);
+            hidro-=2;
+            sem_post(&mutexO);
+            oxy-=1;
+        }
+        else {
+            sem_post(&mutex);
+        }
+
+        sem_wait(&mutexO);
+        printf("O\n");
+        MakeH2O();
+
+        sem_post(&mutex);
+    }
+}
+
+void* hydroFn(void* arg){
+    while(1){
+        sem_wait(&mutex);
+
+        hidro+=1;
+
+        if(hidro>=2 && oxy>=1){
+            sem_post(&mutexH);
+            sem_post(&mutexH);
+            hidro-=2;
+            sem_post(&mutexO);
+            oxy-=1;
+        }
+        else{
+            sem_post(&mutex);
+        }
+
+        sem_wait(&mutexH);
+
+        printf("H\n");
+        MakeH2O();
+        }
+
+}
+
+int main(){
+
+    if(sem_init(&mutex,0,1)==-1){
+        perror("Cannot init semaphore.\n");
+    }
+    if(sem_init(&mutexO,0,0)==-1){
+        perror("Cannot init semaphore.\n");
+    }
+    if(sem_init(&mutexH,0,0)==-1){
+        perror("Cannot init semaphore.\n");
+    }
+
+    pthread_create(&oxyThread,0,oxyFn, NULL);
+    pthread_create(&hidroThread1,0,hydroFn, NULL);
+    pthread_create(&hidroThread2,0,hydroFn, NULL);
+
+    pthread_join(oxyThread, NULL);
+    pthread_join(hidroThread1, NULL);
+    pthread_join(hidroThread2, NULL);
 
     return 0;
 }
